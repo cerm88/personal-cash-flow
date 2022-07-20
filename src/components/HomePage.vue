09f8e869eb3ff8afe36f38cfa4eee0f2f65af21c
cerm88 <cerm1202@hotmail.com>
2022-07-20 02:41:26 -0400

    feat: agregando el resumen de datos de montos

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,7 +6,15 @@
         </template>
         <!-- Slot contents -->
         <template #resume>
-            <ResumeContent />
+            <ResumeContent
+                :total-label="'Ahorro Total'"
+                :date-label="dateLabel"
+                :total-amount="1000"
+                :date-amount="dateAmount"
+            >
+                <template #graphic>Graphic</template>
+                <template #action>Action</template>
+            </ResumeContent>
         </template>
         <!-- Slot movement history -->
         <template #movements>
@@ -20,6 +28,12 @@ import LayoutMain from './LayoutMain.vue';
 import HeaderMain from './HeaderMain.vue';
 import ResumeContent from './ResumeContent.vue';
 import MovementHistory from './MovementHistory.vue';
+
+// Label de fecha dinámica
+const dateLabel = null;
+
+// Monto de fecha dinámico
+const dateAmount = null;
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/ResumeContent.vue
+++ src/components/ResumeContent.vue
@@ -1,7 +1,49 @@
 <template>
-    <main>Resumen</main>
+    <main>
+        <!-- Titulo y monto -->
+        <p>{{ labelVisual }}</p>
+        <h1>{{ amountCurrency }}</h1>
+        <!-- Gráfico -->
+        <div class="graphic">
+            <slot name="graphic"></slot>
+        </div>
+        <!-- Botón -->
+        <div class="action">
+            <slot name="action"></slot>
+        </div>
+    </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { toRefs } from 'vue';
+
+// Función para formatear números a moneda
+const currencyFormater = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+const props = defineProps({
+    // Título con monto de fecha
+    dateLabel: { type: String, default: null },
+    // título con monto total
+    totalLabel: String,
+    // Monto de la fecha específica
+    dateAmount: { type: Number, default: null },
+    // Monto total de los gastos
+    totalAmount: Number,
+});
+
+const { dateLabel, totalLabel, dateAmount, totalAmount } = toRefs(props);
+
+const amountVisual = computed(() =>
+    dateAmount.value !== null ? dateAmount.value : totalAmount.value,
+);
+
+const amountCurrency = computed(() => currencyFormater.format(amountVisual.value));
+
+const labelVisual = computed(() => (dateLabel.value !== null ? dateLabel.value : totalLabel.value));
+</script>
 
 <style lang="scss" scoped></style>
